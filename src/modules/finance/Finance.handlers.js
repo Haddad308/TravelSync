aa7b60e92445915e64bcca714ca39ef6780cc25d
AhmedGaber77 <ahmed255177@gmail.com>
2024-05-14 02:18:50 +0300

    Add some Changes to Accounts module

--- src/modules/finance/Finance.handlers.js
+++ src/modules/finance/Finance.handlers.js
@@ -4,42 +4,39 @@ import Cookies from "js-cookie";
 const token = Cookies.get("userToken");
 
 async function getAccounts(SetAccounts, setIsLoading, id = "") {
-    setIsLoading(true);
-    let data = await instance
-        .get(`/api/accounts${id ? `/${id}` : " "}`, {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        })
-        .catch((error) => {
-            console.error(error);
-            setIsLoading(false);
-        });
-    if (data?.status === 200) {
-        SetAccounts(data.data);
-    }
-    setIsLoading(false);
+  setIsLoading(true);
+  let data = await instance
+    .get(`/api/accounts${id ? `/${id}` : " "}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .catch((error) => {
+      console.error(error);
+      setIsLoading(false);
+    });
+  if (data?.status === 200) {
+    SetAccounts(data.data);
+  }
+  setIsLoading(false);
 }
 
 async function getTransactions(SetTransactions, setIsLoading, id = "") {
-    setIsLoading(true);
-    let data = await instance
-        .get(`/api/accounts/${id ? `${id}` : " "}/transactions`, {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        })
-        .catch((error) => {
-            console.error(error);
-            setIsLoading(false);
-        });
-    if (data?.status === 200) {
-        SetTransactions(data.data);
-    }
-    setIsLoading(false);
+  setIsLoading(true);
+  let data = await instance
+    .get(`/api/accounts/${id ? `${id}/` : ""}transactions`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .catch((error) => {
+      console.error(error);
+      setIsLoading(false);
+    });
+  if (data?.status === 200) {
+    SetTransactions(data.data);
+  }
+  setIsLoading(false);
 }
 
-export {
-    getAccounts,
-    getTransactions
-}
+export { getAccounts, getTransactions };
--- src/modules/finance/pages/Accounts.jsx
+++ src/modules/finance/pages/Accounts.jsx
@@ -4,36 +4,42 @@ import UserBalance from "../components/UserBalance";
 import { getAccounts } from "../Finance.handlers";
 
 export default function Accounts() {
+  const [accounts, setAccounts] = useState([]);
+  const [, setIsLoading] = useState(false);
 
-    const [accounts, setAccounts] = useState([]);
-    const [, setIsLoading] = useState(false)
+  useEffect(() => {
+    getAccounts(setAccounts, setIsLoading);
+  }, []);
 
-    useEffect(() => {
-        getAccounts(setAccounts, setIsLoading)
-    }, [])
-
-
-    return (
-        <div>
-            <div className="m-6 bg-white rounded-3xl" >
-                <div className="p-4" >
-                    <div className="flex flex-col justify-between  px-1" >
-                        <h1 className="font-bold" >Users Balance</h1>
-                        <div className="w-full md:w-72 mt-3">
-                            <Input
-                                label="Search"
-                            // icon={<MagnifyingGlassIcon className="h-5 w-5" />}
-                            />
-                        </div>
-                    </div>
-                    <div className="grid grid-cols-3 mt-5 gap-5" >
-                        {accounts.map(({ id, travelOffice, currentBalance }) => {
-                            return <UserBalance key={id} id={id} AgnecyName={travelOffice?.name} AgnecyEmail={travelOffice?.email} AgencyBalance={currentBalance} />
-                        })}
-                    </div>
-                </div>
+  return (
+    <div>
+      <div className="m-6 bg-white rounded-3xl">
+        <div className="p-4">
+          <div className="flex flex-col justify-between  px-1">
+            <h1 className="font-bold">Users Balance</h1>
+            <div className="w-full md:w-72 mt-3">
+              <Input
+                label="Search"
+                // icon={<MagnifyingGlassIcon className="h-5 w-5" />}
+              />
             </div>
-
+          </div>
+          <div className="grid grid-cols-3 mt-5 gap-5">
+            {accounts.map(({ id, travelOffice, currentBalance }) => {
+              return (
+                <UserBalance
+                  key={id}
+                  id={id}
+                  AgnecyName={travelOffice?.name}
+                  AgnecyEmail={travelOffice?.email}
+                  AgencyBalance={currentBalance}
+                  AgencyProfilePhoto={travelOffice.profilePhoto.imageUrl}
+                />
+              );
+            })}
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
